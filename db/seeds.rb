33bb5f3250ea564c5acccb1ff86cb05352f76745
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-05-13 16:00:23 +0200

    list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Delete every data'
+Flat.destroy_all
+
+puts 'Creating data'
+20.times do
+  restaurant = Flat.new(
+    name: Faker::Movies::StarWars.character,
+    address: Faker::Address.full_address,
+    description: Faker::String.random,
+    price_per_night: Faker::Number.number(digits: 3),
+    number_of_guests: Faker::Number.digit
+  )
+  restaurant.save!
+end
+puts 'Done'
